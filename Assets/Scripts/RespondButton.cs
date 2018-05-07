90cdba111e19623d7879737590e507ea4e39a432
Dicoding Academy <academydicoding@gmail.com>
2018-05-07 11:43:22 +0700

    fixed bug

--- Assets/Scripts/RespondButton.cs
+++ Assets/Scripts/RespondButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class RespondCode : MonoBehaviour
+public class RespondButton : MonoBehaviour
 {
 
     public Image imgButton;
