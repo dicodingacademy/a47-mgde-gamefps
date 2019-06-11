8b4bd00787e65d49de0c55925dd91b9d3ac6a4c3
Agil Julio <88smeksa@gmail.com>
2019-06-11 09:54:39 +0700

    Game Shooter

---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour {
+
+    public void MenuScene() {
+        SceneManager.LoadScene("Menu");
+        Debug.Log("Ke Menu");
+    }
+
+    public void MainScene() {
+        SceneManager.LoadScene("Main");
+        Debug.Log("Mulai Permainan");
+    }
+
+    public void QuitScene() {
+        Application.Quit();
+        Debug.Log("Keluar");
+    }
+}
---
+++ Assets/Scripts/WinningGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinningGame : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.tag == "Player") { 
+            Debug.Log ("Memenangkan Permainan");
+            SceneManager.LoadScene("Finish");
+        }
+    }
+}
